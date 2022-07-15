66a0f905e70ffbaeb796494df829320948e89853
tiberiuiancu <tiberiu.iancu@gmail.com>
2022-07-15 17:57:43 +0200

    added some database functionality

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+db-up:
+	docker run --name mongotest --rm -d mongo \
+		mongo -u
--- common/db.go
+++ common/db.go
@@ -3,35 +3,43 @@ package main
 import (
 	"context"
 	"fmt"
-
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Connection URI
-const uri = "mongodb://user:pass@sample.host:27017/?maxPoolSize=20&w=majority"
-
 type DatabaseConnection struct {
 	uri    string
 	client *mongo.Client
+	db     *mongo.Database
+	// todo: add context
 }
 
-func initDBConn(uri string) (DatabaseConnection, error) {
+func initDBConn(uri string, dbName string) (DatabaseConnection, error) {
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	db := client.Database(dbName)
 
-	if err == nil {
+	if err != nil {
 		return DatabaseConnection{}, err
 	}
 
-	dbConn := DatabaseConnection{
+	return DatabaseConnection{
 		uri,
 		client,
-	}
-
-	return dbConn, nil
+		db,
+	}, nil
 }
 
 func main() {
-	fmt.Println("Successfully connected and pinged.")
+	conn, err := initDBConn("mongodb://root:test@127.0.0.1:27017", "test-db")
+
+	var result bson.M
+	err = conn.db.RunCommand(context.TODO(), bson.D{{"dbStats", 1}}).Decode(&result)
+
+	if err == nil {
+		fmt.Println(result)
+	} else {
+		panic(err)
+	}
 }
