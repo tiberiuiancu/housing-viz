59732f6f0d8a907aa80aa8bfcfc692d4bdb87e03
tiberiuiancu <tiberiu.iancu@gmail.com>
2022-07-31 13:52:54 +0200

    restructuring

--- scraping/main.go
+++ scraping/main.go
@@ -1,18 +1,23 @@
 package main
 
-import "time"
+import (
+	. "housing_viz/common"
+	"housing_viz/scraping/scrapers"
+)
 
 func main() {
-	Scheduler{
-		scrapers: []Scraper{
-			{
-				name:        "Dummy",
-				exec:        dummyScraperRun,
-				channel:     make(chan *Listing),
-				nextRunTime: time.Now(),
-				cooldown:    time.Second * 10,
-				isRunning:   false,
-			},
-		},
-	}.start()
+	//Scheduler{
+	//	scrapers: []Scraper{
+	//		{
+	//			name:        "Dummy",
+	//			exec:        scrapers.DummyScraperRun,
+	//			channel:     make(chan *Listing),
+	//			nextRunTime: time.Now(),
+	//			cooldown:    time.Second * 10,
+	//			isRunning:   false,
+	//		},
+	//	},
+	//}.start()
+
+	scrapers.ParariusScraperRun(make(chan *Listing))
 }
