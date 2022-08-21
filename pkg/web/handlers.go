b1c1082767fd984c0d7464b920cc3bac5197bf47
tiberiuiancu <tiberiu.iancu@gmail.com>
2022-08-21 20:23:25 +0200

    Add handler with database connection

--- pkg/web/handlers.go
+++ pkg/web/handlers.go
@@ -6,18 +6,37 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 var db common.MongoConn
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+type mapInfo struct {
+	MapPoints []mapPoint
+	CenterLat float64
+	CenterLng float64
+	ApiKey    string
+}
+
+func mapHandler(w http.ResponseWriter, r *http.Request) {
+
 	res, _ := db.FindAllMaxN(
-		bson.D{{}},
+		bson.D{{"city", "Amsterdam"}},
 		-1,
 	)
 
-	t, _ := template.ParseFiles("templates/listing.html")
-	t.Execute(w, res)
+	info := mapInfo{
+		convertListingsToMapPoint(res),
+		52.358873,
+		4.861,
+		os.Getenv("MAPS_API_KEY"),
+	}
+
+	// parse and render template
+	t, _ := template.ParseFiles("templates/map.gohtml")
+	if err := t.Execute(w, &info); err != nil {
+		log.Println(err)
+	}
 }
 
 func InitServer() {
@@ -27,6 +46,6 @@ func InitServer() {
 	}
 
 	// add handlers
-	http.HandleFunc("/", homeHandler)
-	log.Fatal("hmm", http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", mapHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
