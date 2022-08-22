d66397d9ac8f2156fa49c672cd008fd233404236
tiberiuiancu <tiberiu.iancu@gmail.com>
2022-08-22 15:05:26 +0200

    heatmap from search bar

--- pkg/web/handlers.go
+++ pkg/web/handlers.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+	"encoding/json"
+	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"housing_viz/pkg/common"
 	"html/template"
@@ -12,30 +14,56 @@ import (
 var db common.MongoConn
 
 type mapInfo struct {
-	MapPoints []mapPoint
-	CenterLat float64
-	CenterLng float64
-	ApiKey    string
+	ApiKey string
+}
+
+type dataRequest struct {
+	LatNE float64 `json:"lat_ne"`
+	LngNE float64 `json:"lng_ne"`
+	LatSW float64 `json:"lat_sw"`
+	LngSW float64 `json:"lng_sw"`
 }
 
 func mapHandler(w http.ResponseWriter, r *http.Request) {
 
-	res, _ := db.FindAllMaxN(
-		bson.D{{"city", "Amsterdam"}},
-		-1,
-	)
-
-	info := mapInfo{
-		convertListingsToMapPoint(res),
-		52.358873,
-		4.861,
-		os.Getenv("MAPS_API_KEY"),
-	}
-
 	// parse and render template
 	t, _ := template.ParseFiles("templates/map.gohtml")
-	if err := t.Execute(w, &info); err != nil {
+	if err := t.Execute(w, &mapInfo{
+		os.Getenv("MAPS_API_KEY"),
+	}); err != nil {
 		log.Println(err)
+	}
+}
+
+func getDataHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("received request", r.Body)
+	response := "[]"
+
+	decoder := json.NewDecoder(r.Body)
+	var req dataRequest
+	if err := decoder.Decode(&req); err != nil {
+		log.Println("Failed to decode", r.Body)
+	} else {
+		// find listings within map area
+		if queryResult, err := db.FindAll(bson.D{
+			{"lat", bson.D{{"$gte", req.LatSW}}},
+			{"lat", bson.D{{"$lte", req.LatNE}}},
+			{"lng", bson.D{{"$gte", req.LngSW}}},
+			{"lng", bson.D{{"$lte", req.LngNE}}},
+		}); err != nil {
+			log.Println("Error while fetching data:", err)
+		} else {
+			mapPoints := convertListingsToMapPoint(queryResult)
+			if res, err := json.Marshal(mapPoints); err != nil {
+				log.Println("Unable to marshal query result:", err)
+			} else {
+				response = string(res)
+			}
+		}
+	}
+
+	if _, writeErr := fmt.Fprint(w, response); writeErr != nil {
+		log.Println("Unable to send response:", writeErr)
 	}
 }
 
@@ -47,5 +75,6 @@ func InitServer() {
 
 	// add handlers
 	http.HandleFunc("/", mapHandler)
+	http.HandleFunc("/data", getDataHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
